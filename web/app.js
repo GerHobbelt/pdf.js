3cbb063c341259258bccdc39bcc1f0545eb48529
Julien ROUSSEAU <jrousseau@doubletrade.com>
2020-01-02 16:58:20 +0100

    Fix hosted viewer origins

--- web/app.js
+++ web/app.js
@@ -1921,7 +1921,7 @@ const PDFViewerApplication = {
 let validateFileURL;
 if (typeof PDFJSDev === "undefined" || PDFJSDev.test("GENERIC")) {
   const HOSTED_VIEWER_ORIGINS = [
-    "http://pdf-viewer.doubletrade.vosao.com/",
+    "http://pdf-viewer.doubletrade.vosao.com",
     "http://localhost:8090",
   ];
   validateFileURL = function validateFileURL(file) {
